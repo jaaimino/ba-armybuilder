8fad28b76aaf37b5aa3fb4e7de27804f6b0f0818
Jacob Aimino <Jacobaimino@gmail.com>
2018-12-12 11:44:46 -0500

    More setup

--- scripts/postbuild.js
+++
@@ -1,8 +0,0 @@
-const moveFile = require('move-file');
-const rimraf = require('rimraf');
-
-rimraf('docs', async () => {
-    await moveFile('build/', 'docs/');
-    console.log('Moving to prod folder');
-})
- 
