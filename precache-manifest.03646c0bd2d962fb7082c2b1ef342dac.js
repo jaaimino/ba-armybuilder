02b14763ebe670a640d384f3bba7bf9c5c2b0f2e
Jacob Aimino <Jacobaimino@gmail.com>
2018-12-12 11:55:14 -0500

    Updates

---
+++ precache-manifest.03646c0bd2d962fb7082c2b1ef342dac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5b183fa6c655da39222",
+    "url": "/ba-armybuilder/static/js/runtime~main.e5b183fa.js"
+  },
+  {
+    "revision": "57c3a6d7083907cddef9",
+    "url": "/ba-armybuilder/static/js/main.57c3a6d7.chunk.js"
+  },
+  {
+    "revision": "e67855e878665b8bc622",
+    "url": "/ba-armybuilder/static/js/1.e67855e8.chunk.js"
+  },
+  {
+    "revision": "57c3a6d7083907cddef9",
+    "url": "/ba-armybuilder/static/css/main.35f2b1dd.chunk.css"
+  },
+  {
+    "revision": "4fef598efe1b8562339c96d136a813bc",
+    "url": "/ba-armybuilder/index.html"
+  }
+];
