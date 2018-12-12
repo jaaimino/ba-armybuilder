a1ec6fe6eaa68bc6719ef8f6ac96002237fe0893
Jacob Aimino <Jacobaimino@gmail.com>
2018-12-12 12:36:20 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ba-armybuilder/precache-manifest.d1e966881f4da3ebbdd80152c14be356.js"
+  "/ba-armybuilder/precache-manifest.17b83c8a7a5424c94b5a7f99e4cba750.js"
 );
 
 workbox.clientsClaim();
