4cd0f3ce9dee0c86283e9e799cd4f418e2b13c91
Jacob Aimino <Jacobaimino@gmail.com>
2018-12-12 23:39:41 -0500

    Initial code for database

--- src/App.js
+++ src/App.js
@@ -6,20 +6,37 @@ import PageNotFound from 'routes/PageNotFound';
 import Breadcrumbs from 'components/Breadcrumbs';
 import MainNav from 'components/MainNav';
 import s from 'styles/app.style';
+import loki from 'lokijs';
+import units from 'data/units';
+import rules from 'data/rules';
+
+const db = new loki('sandbox.db');
+export const LokiContext = React.createContext();
+
+// Make our collections
+const collections = {};
+collections.units = db.addCollection('units');
+collections.rules = db.addCollection('rules');
+
+// Populate with JSON seeds
+collections.units.insert(units);
+collections.rules.insert(rules);
 
 export default function App() {
   return (
     <div style={s.root}>
-      <MainNav />
-      <h1 style={s.title}>Single Page Apps for GitHub Pages</h1>
-      <nav style={s.breadcrumbs}>
-        <Breadcrumbs />
-      </nav>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/example" component={Example} />
-        <Route component={PageNotFound} />
-      </Switch>
+      <LokiContext.Provider value={collections}>
+        <MainNav />
+        <h1 style={s.title}>Single Page Apps for GitHub Pages</h1>
+        <nav style={s.breadcrumbs}>
+          <Breadcrumbs />
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/example" component={Example} />
+          <Route component={PageNotFound} />
+        </Switch>
+      </LokiContext.Provider>
     </div>
   );
 }
--- src/routes/Example.js
+++ src/routes/Example.js
@@ -1,11 +1,42 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
 import s from '../styles/exampleComponent.style';
+import { LokiContext } from 'App';
+import { refs } from 'utils/db';
+console.log(LokiContext);
 
-export default () => (
-  <div style={s.main}>
-    <p style={s.p}>
-      This is an example to you can navigate to.
-    </p>
-  </div>
-);
+export default class Example extends React.Component {
+  // Assign a contextType to read the current theme context.
+  // React will find the closest theme Provider above and use its value.
+  // In this example, the current theme is "dark".
+  static contextType = LokiContext;
+  render() {
+    return (
+      <div style={s.main}>
+        <LokiContext.Consumer>
+          {collections => {
+            const units = collections.units.find().map((unit) => ({
+                    ...unit,
+                    rules: refs(unit.rules, collections.rules, 'name')
+                  }));
+            return (
+              <div>
+                <h1>Dynamically Generated Content from Flat File data in LokiJS DB :O</h1>
+                {units.map((unit, i) => (
+                  <div>
+                    <h1>{unit.name}</h1>
+                    {unit.rules.map((rule, i) => (
+                      <div>
+                        <h3>{rule.name}</h3>
+                        <p>{rule.text}</p>
+                      </div>
+                    ))}
+                  </div>
+                ))}
+              </div>
+            );
+          }}
+        </LokiContext.Consumer>
+      </div>
+    );
+  }
+}
